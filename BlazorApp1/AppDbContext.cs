d4ae2e52cde3d1698aeacef78c44a5810c86ce1b
Student2 <Student2@L-5-17.itstep.local>
2021-12-19 12:44:23 +0300

    AsualCommit

---
+++ BlazorApp1/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp1;
+using Microsoft.EntityFrameworkCore;
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Product => Set<Product>();
+
+    public AppDbContext(
+        DbContextOptions<AppDbContext> options) 
+        : base(options)
+    {
+    }
+}
---
+++ BlazorApp1/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp1;
+
+public class Product
+{
+    public string Name { get; set; }
+    public string Price { get; set; }
+    public int Id { get; set; }
+}
